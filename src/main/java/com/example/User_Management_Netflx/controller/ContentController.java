e9fe0d4c5b8efb83bb0d924b30bf63e0bd4dd7ec
Thusithakit <thusitha@kongcepts.info>
2024-07-09 14:51:12 +0530

    added api functions and endpoints to get all content,get all content with a common query in the title and get content by id.

--- src/main/java/com/example/User_Management_Netflx/controller/ContentController.java
+++ src/main/java/com/example/User_Management_Netflx/controller/ContentController.java
@@ -2,11 +2,14 @@ package com.example.User_Management_Netflx.controller;
 
 import com.example.User_Management_Netflx.dto.ApiResponse;
 import com.example.User_Management_Netflx.dto.ContentDTO;
+import com.example.User_Management_Netflx.dto.UserDTO;
 import com.example.User_Management_Netflx.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/content")
@@ -22,5 +25,18 @@ public class ContentController {
         }else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
+    }
+    @GetMapping("/getAllContent")
+    public List<ContentDTO> getUser(){
+        return contentService.getAllContent();
+    }
+
+    @GetMapping("/getAllContentsByTitle/{title}")
+    public List<ContentDTO> getAllContentsByTitle(@PathVariable String title){
+        return contentService.getAllContentsByTitle(title);
+    }
+    @GetMapping("/getContentById/{contentId}")
+    public ContentDTO getContentById(@PathVariable String contentId){
+        return contentService.getContentById(contentId);
     }
 }
--- src/main/java/com/example/User_Management_Netflx/service/ContentService.java
+++ src/main/java/com/example/User_Management_Netflx/service/ContentService.java
@@ -2,13 +2,17 @@ package com.example.User_Management_Netflx.service;
 
 import com.example.User_Management_Netflx.dto.ApiResponse;
 import com.example.User_Management_Netflx.dto.ContentDTO;
+import com.example.User_Management_Netflx.dto.UserDTO;
 import com.example.User_Management_Netflx.entity.Content;
 import com.example.User_Management_Netflx.entity.User;
 import com.example.User_Management_Netflx.repo.ContentRepo;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -26,6 +30,19 @@ public class ContentService {
             contentRepo.save(modelMapper.map(contentDTO, Content.class));
             return new ApiResponse(true,"Successfully added the Content");
         }
+    }
+    public List<ContentDTO> getAllContent(){
+        List<Content>contentList = contentRepo.findAll();
+        return modelMapper.map(contentList,new TypeToken<List<ContentDTO>>(){}.getType());
+    }
+    public List<ContentDTO> getAllContentsByTitle(String title){
+        List<Content> contentList = contentRepo.getContentsByTitle(title);
+        return modelMapper.map(contentList,new TypeToken<List<ContentDTO>>(){}.getType());
+    }
+
+    public ContentDTO getContentById(String contentId){
+        Content content =contentRepo.getContentById(contentId);
+        return modelMapper.map(content, ContentDTO.class);
     }
 
 }
