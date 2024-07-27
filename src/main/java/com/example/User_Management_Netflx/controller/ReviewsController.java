eaebb350632af97daef8ea175add5461d48bac63
Thusithakit <thusitha@kongcepts.info>
2024-07-27 10:32:35 +0530

    add review save,get and delete methods

--- src/main/java/com/example/User_Management_Netflx/controller/ReviewsController.java
+++ src/main/java/com/example/User_Management_Netflx/controller/ReviewsController.java
@@ -1,12 +1,44 @@
 package com.example.User_Management_Netflx.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.User_Management_Netflx.dto.ApiResponse;
+import com.example.User_Management_Netflx.dto.ReviewsDTO;
+import com.example.User_Management_Netflx.service.ReviewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/reviews")
 @CrossOrigin
 
 public class ReviewsController {
+
+    @Autowired
+    private ReviewsService reviewsService;
+
+    @PostMapping("/saveReview")
+    public ResponseEntity<ApiResponse> saveReview(@RequestBody ReviewsDTO reviewsDTO){
+        ApiResponse response = reviewsService.saveReview(reviewsDTO);
+        if (response.isSuccess()){
+            return ResponseEntity.ok(response);
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+    @GetMapping("/getreviews/{movieId}")
+    public List<ReviewsDTO> getReviews(@PathVariable int movieId){
+        return reviewsService.getReviews(movieId);
+    }
+    @DeleteMapping("/deleteReview/{reviewId}")
+    public ResponseEntity<ApiResponse> deleteContent(@PathVariable Integer reviewId){
+        ApiResponse response = reviewsService.deleteContent(reviewId);
+        if (response.isSuccess()){
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
 }
--- src/main/java/com/example/User_Management_Netflx/repo/ReviewRepo.java
+++ src/main/java/com/example/User_Management_Netflx/repo/ReviewRepo.java
@@ -2,6 +2,11 @@ package com.example.User_Management_Netflx.repo;
 
 import com.example.User_Management_Netflx.entity.Reviews;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepo extends JpaRepository<Reviews, Integer> {
+    @Query(value = "SELECT * FROM reviews WHERE movie_id=?1",nativeQuery = true)
+    List<Reviews> findAllByMovieId(int movieId);
 }
